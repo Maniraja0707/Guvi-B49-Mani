0116be4e84820b4234b7eba56935da67f7f2f312
MANIKANDAN RAJAGOPAL <72884153+Maniraja0707@users.noreply.github.com>
2023-07-04 10:44:25 +0530

    Add files via upload

---
+++ Code Kata/Input Output/09.js
@@ -0,0 +1,31 @@
+//Write a code to get the input in the given format and print the output in the given format.
+
+// Getting input via STDIN
+const readline = require("readline");
+
+const inp = readline.createInterface({
+  input: process.stdin
+});
+
+const userInput=[];
+
+inp.on("line", (data) => {
+  userInput.push(data);
+});
+
+inp.on("close", () => {
+  //start-here
+  //Your code goes here … replace the below line with your code logic 
+let str =userInput[0];
+for (let i=0; i<str.length; i++) {
+    console.log(str[i]+"");
+}
+
+
+//for (let i=0; i<str.length; i++) {
+//    console.log(str[i]+"\n");
+//}
+
+
+  //end-here
+});
